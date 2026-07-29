093352c5a2c83f58fbdca9fe4dbd7d3e49c4a69a
Spider Flash <tun435671@gmail.com>
2026-07-29 07:58:55 +0700

    Fix Render port

--- server.js
+++ server.js
@@ -165,13 +165,12 @@ app.post("/upload", upload.single("video"), (req, res) => {
 
 
 // Server Start
-const server = app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
+const server = app.listen(PORT, () => {
     console.log(
-        "Server running on port 3000"
+        `Server running on port ${PORT}`
     );
-
 });
-
 
 server.timeout = 10 * 60 * 1000;
